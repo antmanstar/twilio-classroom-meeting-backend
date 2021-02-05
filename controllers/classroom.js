c1fdebc300999284824ade927957ed57b40382f1
Kok357357 <huskar357357@gmail.com>
2021-02-05 15:46:06 +0800

    better version

--- controllers/classroom.js
+++ controllers/classroom.js
@@ -135,7 +135,7 @@ exports.createTwilioClassroom = function(req, res) {
                                 .channels
                                 .create({
                                     friendlyName: "welovechannel",
-                                    uniqueName: room.sid,
+                                    uniqueName: data._id,
                                     type: 'public',
                                 })
                                 .then(channel => {
